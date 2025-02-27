5561cd40d2a49f2d59a0e5a4e30be4b1ac423625
Mohammad Junaid <102286744+MJunaid7@users.noreply.github.com>
2025-02-27 11:29:44 +0530

    Add files via upload

---
+++ html_css_js/browsing.js
@@ -0,0 +1,23 @@
+function profilebaropen()
+{
+    
+        if( document.getElementById('profile').style.height== "0vh")
+        {
+            document.getElementById('profile').style.height="40.5vh" 
+        }
+        else {
+            document.getElementById('profile').style.height="0vh" 
+        }
+} 
+
+function socialbaropen()
+{
+    
+        if( document.getElementById('social').style.width== "0vw")
+        {
+            document.getElementById('social').style.width="5vw" 
+        }
+        else {
+            document.getElementById('social').style.width="0vw" 
+        }
+} 
